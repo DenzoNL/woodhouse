03225f521e239a253cc694c69c417c55f868d962
Dennis Bogers <dennis@bogers.xyz>
2025-08-06 22:04:50 +0000

    Sets up basic Axum server
    Initializes the Axum web framework with a basic route.
    
    Adds dependencies for Axum, Serde, Tokio, Tracing and Tracing Subscriber.

---
+++ src/config.rs
@@ -0,0 +1,7 @@
+//! This module contains configuration constants used throughout the application.
+
+/// The name of the application.
+pub const APP_NAME: &str = "Woodhouse";
+
+/// The port on which the app server will listen for incoming connections.
+pub const APP_PORT: u16 = 4242;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(clippy::pedantic)]
+mod config;
+use config::{APP_NAME, APP_PORT};
+
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper
+    let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{APP_PORT}"))
+        .await
+        .unwrap();
+
+    tracing::debug!(
+        "{APP_NAME} is listening on {}",
+        listener.local_addr().unwrap()
+    );
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Sir, I'm afraid that your breakfast will be four minutes late."
 }
